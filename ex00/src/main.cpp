d39b8f32f808f376e3d41d141014bb1d3ac690fe
erijania <erijania@student.42antananarivo.mg>
2025-05-10 14:59:25 +0300

    more test

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -8,5 +8,10 @@ int	main(void)
 	john.attackDamage(10);
 	john.attack("karen");
 	karen.takeDamage(2);
+	karen.beRepaired(12);
+	john.attack("karen");
+	karen.takeDamage(20);
+	karen.takeDamage(10);
+	karen.beRepaired(50);
 	return (0);
 }
